86b922c8d8284d183e1183d63f5145f26ff1c1c8
Hyunjin Cho <jcn09125@gmail.com>
2026-07-12 14:13:01 +0900

    chore: initialize backend project

---
+++ src/test/java/com/example/cnwasshu/CnwasshuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cnwasshu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnwasshuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
